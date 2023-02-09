6d64a5aac7370b2032cd188e7fe224035d1178b4
SARATH <sarath@Saraths-MacBook-Air.local>
2023-02-09 15:18:10 +0530

    cart app

---
+++ src/components.js
@@ -0,0 +1,76 @@
+import {data} from "./data"
+import{useState} from "react"
+import { Button, Card } from "react-bootstrap";
+import "./components.css"
+
+export function Components(){
+    const [card,setCard]= useState(data);
+    const [item,setItems]= useState(0);
+
+    return(
+        <div>
+            <div className="heading">
+                <h1>Shop with style</h1>
+                <h2>With shop homepage templates</h2>
+                <h3>CART:{item}</h3>
+            </div>
+            <div className="container">
+                {card.map((prod,idx)=>(
+                    <CardDetails
+                    key={idx}
+                    title={prod.title}
+                    ratings={prod.ratings}
+                    price={prod.price}
+                    specialPrice={prod.specialPrice}
+                    buttonName={prod.buttonName}
+                    changedName={prod.changedName}
+                    setCard={setCard}
+                    setItems={setItems}
+                    idx={idx}
+                    item={item}
+                    card={card}
+
+                    />
+                ))}
+                
+            </div>
+        </div>
+    )}
+
+function CardDetails ({setCard, setItems, idx , item,title,ratings,price,specialPrice,buttonName,changedName}) {
+    let count = 0
+     const [showBtn, setShowBtn] = useState(true);
+     const handleCartAdd = (idx, setItems, item) => {
+         setShowBtn(!showBtn)
+         setItems(item + 1)
+  
+     }
+     const handleCartRemove = (idx, setItems , item, setCard) =>{
+        setShowBtn(!showBtn)
+        setItems(item - 1)
+    
+     }
+return(
+<div className="main-card">
+     <div className="Card">
+        <Card style={{ width: '17rem',margin:"20px" ,height:"25rem"}} >
+    <Card.Body style={{padding:"10px"}}>
+      <Card.Title>{title}</Card.Title>
+      <p ><s>{price}</s> Special Price:{specialPrice}</p>
+      <p className="rating-data">{ratings}</p>
+      <div className="btn-area">
+        
+          {buttonName == "Add to cart" ? showBtn ? <Button  className="btn" onClick={()=>handleCartAdd(idx, setItems, item)} variant="primary">{buttonName}</Button>: 
+          <Button className="btn" onClick={()=>handleCartRemove(idx, setItems, item , setCard)} variant="secondary">{changedName}</Button>  :<Button className="btn" >{buttonName}</Button>
+        }
+        
+        </div>
+    </Card.Body>
+   
+  </Card>
+
+</div>
+   
+    </div>
+)
+}
---
+++ src/data.js
@@ -0,0 +1,67 @@
+export let data=[
+    {
+        title:"Fancy Products",
+        ratings:"",
+        price:"$50",
+        specialPrice:"$20 - $35",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart"
+    },
+    {
+        title:"Special Products",
+        ratings:"⭐️⭐️⭐️⭐️⭐️",
+        price:"$20",
+        specialPrice:"$18",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart" 
+    },
+    {
+        title:"Sale Products",
+        ratings:"⭐️⭐️⭐️",
+        price:"$70",
+        specialPrice:"$40",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart"
+    },
+    {
+        title:"Popular Products",
+        ratings:"⭐️⭐️⭐️⭐️",
+        price:"$40",
+        specialPrice:"$35",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart" 
+    },
+    {
+        title:"Fancy Products",
+        ratings:"",
+        price:"$45",
+        specialPrice:"$20 - $35",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart"
+    },
+    {
+        title:"Special Products",
+        ratings:"⭐️⭐️⭐️⭐️⭐️",
+        price:"$20",
+        specialPrice:"$18",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart" 
+    },
+    {
+        title:"Sale Products",
+        ratings:"⭐️⭐️⭐️",
+        price:"$55",
+        specialPrice:"$40",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart"
+    },
+    {
+        title:"Popular Products",
+        ratings:"⭐️⭐️⭐️⭐️",
+        price:"$40",
+        specialPrice:"$35",
+        buttonName:"Add to cart",
+        changedName:"Remove from cart" 
+    }
+    
+]
